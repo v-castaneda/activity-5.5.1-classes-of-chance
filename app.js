932ea4358ad42350b3727c98c994a1416f005f58
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-06-25 11:32:27 -0400

    updating apps.js

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ class Casino {
     if ((Math.random() <= 0.5) | (this.isFakeCoin === true)) {
       console.log(`Casino ${this.name} Wins!`);
     } else {
-      this.timesPlayed + 1;
+      //   this.timesPlayed + 1;
       console.log("Player wins!");
       console.log(`You have won ${betAmount * (this.timesPlayed + 1)}`);
     }
   }
+  rollDie(d) {
+    const numRolled = Math.random();
+    console.log();
+  }
 }
 
-// TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+// // TESTS
+// const myCasino = new Casino("HackerU Casino");
+// console.log(myCasino);
+// myCasino.playGame(5);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
-// BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
+// // BONUS TESTS
+// const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+// console.log(myBonusCasino);
+// myBonusCasino.playGame(5);
+// myBonusCasino.playGame(15);
+// myBonusCasino.playGame(25);
+// myBonusCasino.playGame(35);
 
 // Extra BONUS TESTS
-/*
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
